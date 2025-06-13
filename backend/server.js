bd26fcb72ddb9161b2e57db00b99647f4e75ddc1
SumitGarg <garg10sumit@gmail.com>
2025-06-13 21:40:10 +0530

    Update CORS config to allow frontend with restricted methods and headers

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const app = express();
+app.use(
+    cors({
+        origin:process.env.CLIENT_URL || "*",
+        methods: ["GET","POST","PUT","DELETE"],
+        allowedHeaders: ["Content-Type" , "Authorization"]
+    })
+)
