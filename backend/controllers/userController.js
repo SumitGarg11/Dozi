50089fc73ce3e1ff1e8826890623db0fca509f95
SumitGarg <garg10sumit@gmail.com>
2025-06-16 16:52:08 +0530

    feat: add user registration with role and password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,34 +1,72 @@
-const User = require('../model/User');
-const bcrypt = require('bcryptjs')
-const jwt = require("jsonwebtoken")
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const generateToken = (userId) => {
-    return jwt.sign({ id: userId}, process.env.JWT_SECRET,{expiresIn: "7d"});
-}
-
-
-const registerUser = async(req,res) => {
-    try {} catch (error) {
-        res.status(500).json({message: "Server error",error:error.message});
-    }
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
-const loginUser = async(req,res) => {
-    try {} catch (error) {
-        res.status(500).json({message: "Server error",error:error.message});
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl, adminInviteToken } =
+      req.body;
+    // check if user already exists
+    const userExits = await User.findOne({ email });
+    if (userExits) {
+      return res.status(400).json({ message: "User already exists" });
     }
+    // determine user role : Admin if correct token is provided, otherwise member
+    let role = "member";
+    if (
+      adminInviteToken &&
+      adminInviteToken == process.env.ADMIN_INVITE_TOKEN
+    ) {
+      role = "admin";
+    }
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // create new user
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+      role,
+    });
+    // return user data with jwt
+    res.status(201).json({
+      __id: user._id,
+      name: user.name,
+      email: user.role,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
-const getUserProfile = async(req,res) => {
-    try {} catch (error) {
-        res.status(500).json({message: "Server error",error:error.message});
-    }
+const loginUser = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
-const updateUserProfile = async(req,res) => {
-    try {} catch (error) {
-        res.status(500).json({message: "Server error",error:error.message});
-    }
+const getUserProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
-module.exports = {registerUser, loginUser, getUserProfile, updateUserProfile }
+const updateUserProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser, getUserProfile, updateUserProfile };
