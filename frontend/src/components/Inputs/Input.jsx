20ef3ee5f1295f3119673ceb2d8e9dc468e8f983
SumitGarg <garg10sumit@gmail.com>
2025-06-18 20:08:33 +0530

    feat(auth): add login page and authentication layout with image setup

---
+++ frontend/src/components/Inputs/Input.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Input({ value, onChange, label, placeholder, type }) {
+  return (
+    <div>
+      <label className="text-[13px] text-slate-800 ">{label}</label>
+      <div className="input-box">
+        <input
+          type={
+            type == "password" ? (showPassword ? "text" : "password") : type
+          }
+          placeholder={placeholder}
+          className="w-full bg-transparent outline-none "
+          value={value}
+          onChange={(e) => onChange(e)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Input;
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>Login</div>
-  )
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col  justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to Log IN
+        </p>
+        <form onSubmit={handleLogin}>
+          <Input value={email} onChange={({target}) => setEmail(target.email)} label="Email Address" placeholder="john@example.com" type="text" />
+          
+        </form>
+      </div>
+    </AuthLayout>
+  );
 }
 
-export default Login
+export default Login;
