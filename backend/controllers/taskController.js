40e99d478ffff37bd0fb9618c9bf353a037e1319
SumitGarg <garg10sumit@gmail.com>
2025-06-18 13:19:32 +0530

    update: checklist update logic with progress & status auto-sync

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -165,6 +165,33 @@ const updateTaskChecklist = async (req, res) => {
   try {
     const {todoChecklist} = req.body;
     const task = await Task.findById(req.params.id);
+
+    if(!task) return res.status(404).json({message: "Task not found"});
+    if(!task.assignedTo.includes(req.user._id) && req.user.role !== "admin"){
+      return res.status(403).json({message: "Not authorized to update checklist"});
+    }
+    task.todoChecklist = todoChecklist; // replace with updated checklist
+    const completedCount = todo.todoChecklist.filter(
+      (item) => item.completed
+    ).length;
+    
+    const totalItems = task.todoChecklist.length;
+    task.progress = totalItems > 0 ? Math.round((completedCount / totalItems) * 100) : 0;
+    if(task.progress === 100){
+      task.status = "Completed";
+    }
+    else if(task.progress > 0){
+      task.status = "In Progress";
+    }
+    else {
+      task.status = "Pending";
+    }
+    await task.save();
+    const updatedTask = await Task.findById(req.params.id).populate(
+      "assignedTo",
+      "name email profileImageUrl"
+    );
+    res.json({message: "Task checklist updated", task:updateTask});
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
